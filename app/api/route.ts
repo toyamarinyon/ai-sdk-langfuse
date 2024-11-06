01222fc2c51f1ad2ee53946be5e3cb233dbae5c2
toyamarinyon <toyamarinyon@gmail.com>
2024-11-06 23:29:14 +0900

    langfuse

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,6 +5,9 @@ export async function POST() {
   const { text } = await generateText({
     model: openai("gpt-4o-mini"),
     prompt: "hello",
+    experimental_telemetry: {
+      isEnabled: true,
+    },
   });
   return Response.json({ text });
 }
